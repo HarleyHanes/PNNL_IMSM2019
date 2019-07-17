01b86811d7a38a72d92a226dd68c800a772465e1
jennasjunneson <51933742+jennasjunneson@users.noreply.github.com>
2019-07-17 10:51:25 -0400

    Add files via upload

--- MatPower/NewNetwork.m
+++ MatPower/NewNetwork.m
@@ -1,22 +1,29 @@
 function [Cost,Network] = NewNetwork(m,iter,perturb)
 %NewNetwork - making perturbations of the data in the power cases
 
-Network=cell(iter,3);
+% m = case number
+% iter = how many iterations we choose to run - how many different
+% perturbations we want
 
-morig=m;
+% perturb = a two entry vector with the standard deviation and variation of
+% a probability distribution for perturbing the values
 
-[j,k]=size(m.bus);
+Network=cell(iter,3); % the cell array to hold the results
 
-pertval=randn(j,iter)*perturb(2)+perturb(1);
+morig=m; % hold the original case file
+
+[j,k]=size(m.bus); % find the size of the m.bus in the case file
+
+pertval=randn(j,iter)*perturb(2)+perturb(1); % create the perturbation vector
 
 
 for i=1:iter
-    m=morig;
-    m.bus(:,[3:4])=m.bus(:,[3,4])+pertval(i)*m.bus(:,[3,4]);
-    Network{i,1}=pertval(i);
-    Network{i,2}=m.bus;
-    results=runopf(m);
-    Network{i,3}=results.f;
+    m=morig; %reset to the original m case
+    m.bus(:,[3:4])=m.bus(:,[3,4])+pertval(i)*m.bus(:,[3,4]); %perturb m.bus
+    Network{i,1}=pertval(i); %store perturbation value for this iteration
+    Network{i,2}=m.bus; %store m.bus for this iteration 
+    results=runopf(m); %find the results for this iteration
+    Network{i,3}=results.f; %store the total cost in cell array
 end
 
 
