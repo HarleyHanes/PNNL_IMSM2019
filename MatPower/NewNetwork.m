7b69dc6f72dc518abce75f4452124f9131494b56
jennasjunneson <51933742+jennasjunneson@users.noreply.github.com>
2019-07-17 16:12:27 -0400

    Add files via upload

--- MatPower/NewNetwork.m
+++ MatPower/NewNetwork.m
@@ -7,8 +7,7 @@ function [NetworkO] = NewNetwork(m,iter,perturb)
 % perturb = a two entry vector with the standard deviation and variation of
 % a probability distribution for perturbing the values
 
-NetworkO=cell(iter,4); % the cell array to hold the results for opf
-NetworkC=cell(iter,4); % the cell array to hold the results for cpf
+Network=cell(iter,4); % the cell array to hold results for opf
 
 morig=m; % hold the original case file
 
@@ -19,17 +18,18 @@ pertval=randn(j,iter)*perturb(2)+perturb(1); % create the perturbation vector
 
 for i=1:iter
     m=morig; %reset to the original m case
-    m.bus(:,[3:4])=pertval(i)*m.bus(:,[3,4]); %perturb m.bus
+    for n=1:j
+        pertval3=randn()*perturb(2)+perturb(1); % create the perturbation vector
+        pertval4=randn()*perturb(2)+perturb(1); % create the perturbation vector
+        m.bus(n,3)=pertval3*m.bus(n,3); % perturb m.bus column 3
+        m.bus(n,4)=pertval4*m.bus(n,4); % perturb m.bus column 4
+    end
+    
     resultsO=runopf(m); %find the results for this iteration for opf
-    resultsC=runcpf(m); %find the results for this iteration for cpf
-    NetworkO{i,1}=pertval(i); %store perturbation value for this iteration
-    NetworkO{i,2}=m.bus; %store m.bus for this iteration 
-    NetworkO{i,3}=results.f; %store the total cost in cell array for opf
-    NetworkO{i,4}=results.success; %record if runopf converged or not; 1=converge
-    NetworkC{i,1}=pertval(i); %store perturbation value for this iteration
-    NetworkC{i,2}=m.bus; %store m.bus for this iteration 
-    NetworkC{i,3}=results.f; %store the total cost in cell array for opf
-    NetworkC{i,4}=results.success; %record if runopf converged or not; 1=converge
+    Network{i,1}=pertval(i); %store perturbation value for this iteration
+    Network{i,2}=m.bus; %store m.bus for this iteration 
+    Network{i,3}=resultsO.f; %store the total cost in cell array for opf
+    Network{i,4}=resultsO.success; %record if runopf converged or not; 1=converge
 
 end
 
