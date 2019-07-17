448ecc12c29b5c7b78dbdce8e0d3caaeb3efb762
Yuqi Su <suyuqi111@gmail.com>
2019-07-17 11:04:54 -0400

    Add files via upload

--- MatPower/test_opf.m
+++ MatPower/test_opf.m
@@ -1,17 +1,16 @@
 
-%%%graph
 
+%%%graph
+function yyy= cost(x,y)
 xa=Network(:,1);
-ya=Network(:,2);
+ya=Network(:,3);
 yyy=cost(xa,ya);
 
-function yyy= cost(x,y)
 figure(1)
 plot(x,y)
 title('Figure of pertubation and cost');
 xlabel('pertubation');
 ylabel('cost');
-
 
 figure(2)
 h = histogram2(x,y);
