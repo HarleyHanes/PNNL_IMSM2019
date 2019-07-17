857fd49aab0051574283e32cc637ab3b912d9bd6
jennasjunneson <51933742+jennasjunneson@users.noreply.github.com>
2019-07-17 16:49:05 -0400

    Add files via upload

--- MatPower/NewNetwork.m
+++ MatPower/NewNetwork.m
@@ -1,4 +1,4 @@
-function [NetworkO] = NewNetwork(m,iter,perturb)
+function [results,Network] = NewNetwork(m,iter,perturb)
 %NewNetwork - making perturbations of the data in the power cases
 
 % m = case number
@@ -7,14 +7,11 @@ function [NetworkO] = NewNetwork(m,iter,perturb)
 % perturb = a two entry vector with the standard deviation and variation of
 % a probability distribution for perturbing the values
 
-Network=cell(iter,4); % the cell array to hold results for opf
+Network=cell(iter,5); % the cell array to hold results for opf
 
 morig=m; % hold the original case file
 
 [j,k]=size(m.bus); % find the size of the m.bus in the case file
-
-pertval=randn(j,iter)*perturb(2)+perturb(1); % create the perturbation vector
-
 
 for i=1:iter
     m=morig; %reset to the original m case
@@ -25,11 +22,12 @@ for i=1:iter
         m.bus(n,4)=pertval4*m.bus(n,4); % perturb m.bus column 4
     end
     
-    resultsO=runopf(m); %find the results for this iteration for opf
-    Network{i,1}=pertval(i); %store perturbation value for this iteration
-    Network{i,2}=m.bus; %store m.bus for this iteration 
-    Network{i,3}=resultsO.f; %store the total cost in cell array for opf
-    Network{i,4}=resultsO.success; %record if runopf converged or not; 1=converge
+    results=runopf(m); %find the results for this iteration for opf
+    Network{i,1}=results; %store perturbation value for this iteration
+    Network{i,2}=m; %store m for this iteration 
+    Network{i,3}={results.gen(:,2),results.gen(:,6)}; % record changes in P and V 
+    Network{i,4}=results.f; %store the total cost in cell array for opf
+    Network{i,5}=results.success; %record if runopf converged or not; 1=converge
 
 end
 
