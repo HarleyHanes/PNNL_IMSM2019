e21a2058ba9981262c050b91e54346ede6cac91d
jennasjunneson <51933742+jennasjunneson@users.noreply.github.com>
2019-07-20 15:09:29 -0400

    Create figures analyzing generated data in case 30

--- MatPower/PlotDataSpecificPoints.m
+++ MatPower/PlotDataSpecificPoints.m
@@ -15,8 +15,19 @@ close all
 % section with the subplots.
 
 tic
+
+% 30 case
 cf=9201;  % # of total files in the failure folder
 cs=20797; % # of total files in the success folder
+F=('/Users/jennasjunneson/Desktop/Stuff/RIT Degree/Workshops2019/Failure');
+S=('/Users/jennasjunneson/Desktop/Stuff/RIT Degree/Workshops2019/Success');
+
+% 300 case
+% cf=4565;  % # of total files in the failure folder
+% cs=5000; % # of total files in the success folder
+% F=('/Users/jennasjunneson/Desktop/Stuff/RIT Degree/Workshops2019/case300data/Failure');
+% S=('/Users/jennasjunneson/Desktop/Stuff/RIT Degree/Workshops2019/Success');
+
 
 % ATTENTION:  To run this code on your own computer, the following "F" and
 % "S" must follow the location on your computer for these files.  You must
@@ -24,8 +35,7 @@ cs=20797; % # of total files in the success folder
 
 NetF=cell(cf+1,1);
 NetS=cell(cs+1,1);
-F=('/Users/jennasjunneson/Desktop/Stuff/RIT Degree/Workshops2019/Failure');
-S=('/Users/jennasjunneson/Desktop/Stuff/RIT Degree/Workshops2019/Success');
+
 
 for i=0:cf
     f=sprintf('NormTestFail%d',i);
@@ -78,17 +88,17 @@ end
 figure
 scatterF=scatter(cell2mat(XCol3F(1:25:end)), cell2mat(YCol4F(1:25:end)),'r');
 scatterF.MarkerEdgeAlpha=0.2;
-title('p versus q values for all buses');
+% title('p versus q values for all buses','FontSize',22);
 hold on
-vline(pmn,'k','maximum p min for all gen')
-hline(qmx,'r','minimum q max for all gen')
-hline(qmn,'r','maximum q min for all gen')
-vline(pmx,'k','minimum p max for all gen')
+% vline(pmn,'k','maximum p min for all bus')
+% hline(qmx,'r','minimum q max for all bus')
+% hline(qmn,'r','maximum q min for all bus')
+% vline(pmx,'k','minimum p max for all bus')
 scatterS=scatter(cell2mat(XCol3S(1:25:end)), cell2mat(YCol4S(1:25:end)),'b');
 scatterS.MarkerEdgeAlpha=0.2;
-legend('fail values','suc values')
-xlabel('p values');
-ylabel('q values');
+legend({'fail values','suc values'},'Location','northwest','FontSize',14)
+xlabel('p values','FontSize',16);
+ylabel('q values','FontSize',16);
 
 %% Divide data into buses and graph each bus
 
@@ -163,9 +173,9 @@ X=1:n;
 % plot(X,pminF,'r');
 % plot(X,qminF,'k');
 % legend('Fail p','Fail q');
-% title('Failures p and q max and min values');
-% xlabel=('Bus Number');
-% ylabel=('Value');
+% title('Failures p and q max and min values','FontSize',22);
+% xlabel=('Bus Number','FontSize',16);
+% ylabel=('Value','FontSize',16);
 % 
 % figure
 % plot(X,pmaxS,'b');
@@ -174,62 +184,62 @@ X=1:n;
 % plot(X,pminS,'b');
 % plot(X,qminS,'g');
 % legend('Suc p','Suc q');
-% title('Successes p and q max and min values');
-% xlabel=('Bus Number');
-% ylabel=('Value');
+% title('Successes p and q max and min values','FontSize',22);
+% xlabel=('Bus Number','FontSize',16);
+% ylabel=('Value','FontSize',16);
 
 
 YF=[pmaxF pminF qmaxF qminF];
 figure
 bar(YF);
 legend('Fail p max','Fail p min','Fail q max','Fail q min');
-title('Failure p and q max and mins');
-xlabel('Bus Number');
-ylabel('value');
+title('Failure p and q max and mins','FontSize',22);
+xlabel('Bus Number','FontSize',16);
+ylabel('value','FontSize',16);
 
 YS=[pmaxS pminS qmaxS qminS];
 figure
 bar(YS);
 legend('Suc p max','Suc p min','Suc q max','Suc q min');
-title('Success p and q max and mins');
-xlabel('Bus Number');
-ylabel('value');
+title('Success p and q max and mins','FontSize',22);
+xlabel('Bus Number','FontSize',16);
+ylabel('value','FontSize',16);
 
 figure
 plot(X,pmaxF,'r')
 hold on
 plot(X,pmaxS,'b')
 legend('Fail p max','Suc p max')
-title('p max values')
-xlabel('Bus Number');
-ylabel('value');
+title('p max values','FontSize',22)
+xlabel('Bus Number','FontSize',16);
+ylabel('value','FontSize',16);
 
 figure
 plot(X,pminF,'r')
 hold on
 plot(X,pminS,'b')
 legend('Fail p min','Suc p min')
-title('p min values')
-xlabel('Bus Number');
-ylabel('value');
+title('p min values','FontSize',22)
+xlabel('Bus Number','FontSize',16);
+ylabel('value','FontSize',16);
 
 figure
 plot(X,qmaxF,'r')
 hold on
 plot(X,qmaxS,'b')
 legend('Fail q max','Suc q max')
-title('q max values')
-xlabel('Bus Number');
-ylabel('value');
+title('q max values','FontSize',22)
+xlabel('Bus Number','FontSize',16);
+ylabel('value','FontSize',16);
 
 figure
 plot(X,qminF,'r')
 hold on
 plot(X,qminS,'b')
 legend('Fail q min','Suc q min')
-title('q min values')
-xlabel('Bus Number');
-ylabel('value');
+title('q min values','FontSize',22)
+xlabel('Bus Number','FontSize',16);
+ylabel('value','FontSize',16);
 
 %% Plot the cost for each of the functions in failures and successes
 
@@ -275,6 +285,8 @@ end
 
 figure
 scatterC=scatter(XF,YcostF,'r');
+% xlim([0 5500])
+xlim([0 26000])
 hold on
 scatterC2=scatter(XS,YcostS,'b');
 scatterC.MarkerEdgeAlpha=0.2;
@@ -283,13 +295,18 @@ hline(CFmax,'r');
 hline(CFmin,'r');
 hline(CSmax,'b');
 hline(CSmin,'b');
-title('Cost of failures(red) and successes(blue)');
-text(22000,775,['Fail Max $',num2str(round(CFmax,2))],'Color','r')
-text(22000,579,['Fail Min $',num2str(round(CFmin,2))],'Color','r')
-text(22000,681,['Suc Max $',num2str(round(CSmax,2))],'Color','b')
-text(22000,465,['Suc Min $',num2str(round(CSmin,2))],'Color','b')
-xlabel('Simulation Number')
-ylabel('Cost')
+% title('Cost of failures(red) and successes(blue)','FontSize',22);
+text(22000,775,['Fail Max $',num2str(round(CFmax,2))],'Color','r','FontSize',14)
+text(22000,579,['Fail Min $',num2str(round(CFmin,2))],'Color','r','FontSize',14)
+text(22000,681,['Suc Max $',num2str(round(CSmax,2))],'Color','b','FontSize',14)
+text(22000,465,['Suc Min $',num2str(round(CSmin,2))],'Color','b','FontSize',14)
+% text(4800,790000,['Fail Max $',num2str(round(CFmax,2))],'Color','r','FontSize',14)
+% text(4800,690000,['Fail Min $',num2str(round(CFmin,2))],'Color','r','FontSize',14)
+% text(4800,764000,['Suc Max $',num2str(round(CSmax,2))],'Color','b','FontSize',14)
+% text(4800,674000,['Suc Min $',num2str(round(CSmin,2))],'Color','b','FontSize',14)
+xlabel('Simulation Number','FontSize',16)
+ylabel('Cost per Hour','FontSize',16)
+legend({'fail values','suc values'},'Location','north','FontSize',14)
 
 
 
